c70906693f37cc711116b37d0ae0cf08d442b6fa
davidiqm <al061737@uacam.mx>
2020-10-16 00:56:55 -0500

    Se agrego la conexion a la base de datos remota y una clase de configuracion

--- src/main/java/Modelo/ConnectionBD.java
+++ src/main/java/Modelo/ConnectionBD.java
@@ -1,9 +1,30 @@
 package Modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Angel J Garcia Sandoval & David I Quime Montalvo
  */
 public class ConnectionBD {
     
+    public ConnectionBD() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String connectionURL = "jdbc:sqlserver://sql5059.site4now.net\\SQLEXPRESS:1433;databaseName=DB_A68F20_chambipa;"
+                    + "user=DB_A68F20_chambipa_admin;password=raizdemenos1";
+            Connection con = DriverManager.getConnection(connectionURL);
+            System.out.println("Conectado");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionBD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
--- src/main/java/Vista/Main.java
+++ src/main/java/Vista/Main.java
@@ -1,11 +1,13 @@
 package Vista;
 
+import Controlador.Configuration;
+
 /**
  *
  * @author Angel J Garcia Sandoval & David I Quime Montalvo
  */
 public class Main {
     public static void main(String args[]) {
-        Login interfaz = new Login();
+            Configuration conf = new Configuration();
     }
 }
